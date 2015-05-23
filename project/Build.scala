5babeab37afe8e3dbc4896ac6584388c2edbc6d5
Jan Prach <jendap@gmail.com>
2015-05-23 13:43:55 -0700

    cleanup sbt and update sbt-assembly

--- project/Build.scala
+++ project/Build.scala
@@ -1,29 +1,23 @@
-// I would personally prefer sbt to use these scala files.
-// But even the latest sbt documentation still explicitly
-// prefer *.sbt files.
-
-/*
 import sbt._
 import Keys._
-import sbtassembly.Plugin._
-import AssemblyKeys._
+import sbtassembly.AssemblyPlugin.autoImport._
 
 object ScalaBootBuild extends Build {
-  lazy val buildSettings = Defaults.defaultSettings ++ Seq(
+  lazy val commonSettings = Seq(
     organization := "com.janprach.scala-boot",
     version := "0.0.1-SNAPSHOT",
-    scalaVersion := "2.11.4",
+    scalaVersion := "2.11.6",
     libraryDependencies ++= Seq(
-      "org.scalatest" %% "scalatest" % "2.2.2" % "test"
+      "org.scalatest" %% "scalatest" % "2.2.4" % "test"
     )
   )
 
   lazy val scalaBoot = Project(
     id = "scala-boot",
     base = file("."),
-    settings = buildSettings ++ assemblySettings ++ Seq(
+    settings = commonSettings ++ Seq(
       // additional custom settings
+      updateOptions := updateOptions.value.withCachedResolution(true)
     )
   )
 }
-*/
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,3 @@
-addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.9.0")
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.13.0")
+
+addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.1.8")
