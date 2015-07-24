1e2ee5b395fcfc13c45a112e63f5113cb761e655
Jan Prach <jendap@gmail.com>
2015-07-24 14:00:11 -0700

    remove eclipse installation scripts

--- support/run-eclipse.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-ECLIPSE_VERSION="mars"
-ECLIPSE_RELEASE="R"
-
-eclipse_home="${1:-$HOME/eclipse.$ECLIPSE_VERSION.$ECLIPSE_RELEASE.scala}"
-
-$eclipse_home/eclipse -data $eclipse_home/workspace
